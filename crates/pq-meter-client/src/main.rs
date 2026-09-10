a807c28b2d993041c9586e3a0c3be2abad917439
chris <chris.jacomet6@gmail.com>
2026-09-10 17:48:53 +0200

    update monitorfunction

--- crates/pq-meter-client/src/main.rs
+++ crates/pq-meter-client/src/main.rs
@@ -192,20 +192,17 @@ async fn monitor(client: &mut Umg605ProClient, period: Duration) -> anyhow::Resu
         interval.tick().await;
         let start = Instant::now();
         
+        let systime = client.systime().await?;
+        let frequency = client.frequency().await?;
+        
         let voltage_l1 = client.voltage_l1().await?;
         let current_l1 = client.current_l1().await?;
-        let power_l1_n = client.power_l1_n().await?;
+        let real_power_l1 = client.real_power_l1().await?;
         let apparent_power_l1 = client.apparent_power_l1().await?;
         let reactive_power_l1 = client.reactive_power_l1().await?;
-        let cosphi_l1 = client.cosphi_l1().await?;
-        let thd_voltage_l1 = client.thd_voltage_l1().await?;
+        let cos_phi_l1 = client.cos_phi_l1().await?;
+        let real_energy_consumed_l1 = client.real_energy_consumed_l1().await?;
         let thd_current_l1 = client.thd_current_l1().await?;
-        
-        let frequency = client.frequency().await?;
-        let current_sum = client.current_sum().await?;
-        let real_power_sum = client.real_power_sum().await?;
-        let apparent_power_sum = client.apparent_power_sum().await?;
-        let reactive_power_sum = client.reactive_power_sum().await?;
         
         let total_elapsed = start.elapsed();
 
@@ -217,9 +214,8 @@ async fn monitor(client: &mut Umg605ProClient, period: Duration) -> anyhow::Resu
         }
 
         println!(
-            "L1 [U: {:.2}V, I: {:.2}A, P: {:.2}W, S: {:.2}VA, Q: {:.2}var, PF: {:.2}, THD_U: {:.2}%, THD_I: {:.2}%] | Global [f: {:.2}Hz, I_sum: {:.2}A, P_sum: {:.2}W, S_sum: {:.2}VA, Q_sum: {:.2}var]",
-            voltage_l1, current_l1, power_l1_n, apparent_power_l1, reactive_power_l1, cosphi_l1, thd_voltage_l1, thd_current_l1,
-            frequency, current_sum, real_power_sum, apparent_power_sum, reactive_power_sum
+            "Time: {} | Freq: {:.2}Hz | L1 [U: {:.2}V, I: {:.2}A, P: {:.2}W, S: {:.2}VA, Q: {:.2}var, PF: {:.2}, Energy: {:.2}Wh, THD_I: {:.2}%]",
+            systime, frequency, voltage_l1, current_l1, real_power_l1, apparent_power_l1, reactive_power_l1, cos_phi_l1, real_energy_consumed_l1, thd_current_l1
         );
     }
 }
